6a4917d2e917c11a9474fa7d42370d66b09b0f71
narumichiaki <amatory-kudzu-0w@icloud.com>
2024-10-25 15:09:01 +0900

    Backendのうち、Routerなどの基幹部分をhelperに分離

--- backend.py
+++ backend.py
@@ -1,10 +1,9 @@
-from typing import Callable, Any
-from dataclasses import dataclass
+from typing import Any
 import sys
 import logging
 import js
-import json
 from pydantic import BaseModel, field_validator
+from backend_helper import APP, Response, api_endpoint
 
 logging.basicConfig(stream = sys.stdout)
 LOGGER: logging.Logger = logging.getLogger(__name__)
@@ -16,59 +15,10 @@ LOGGER.setLevel(logging.DEBUG)
 
 
 ### CONTROLLER ###
-# API Router（APIをデコレータで表現できるようにしつつ、バグ予防のためにAPIから受け取る値をチェックする）
-class Router:
-    def __init__(self):
-        self.routes: dict[str, tuple[Callable, type[BaseModel] | None]] = {}
-
-    def route(self, path: str, model: type[BaseModel] | None = None) -> Any:
-        def decorator(func: Callable):
-            # パス、メソッド、関数、およびPydanticモデルを登録
-            self.routes[path] = (func, model)
-            return func
-        return decorator
-
-    def call_route(self, path: str, json_param: dict[str, Any]) -> Any:
-        route_info = self.routes.get(path)
-        if route_info:
-            route_func, model = route_info
-            # Pydanticを用いて入力をチェック
-            if model:
-                try:
-                    data = model(**json_param)
-                    return route_func(data)
-                except ValueError as e:
-                    return Response.bad_request(f"入力データのフォーマットがAPI\"{path}\"の仕様と整合しません。： {str(e)}")
-            else:
-                return route_func(json_param)
-        else:
-            return Response.bad_request(f"指定されたパス\"{path}\"が見つかりません。")
-APP = Router()
-
-# レスポンスを表すクラス
-@dataclass
-class Response:
-    status_code: int
-    content: dict[str, Any]
-
-    @classmethod
-    def ok(cls, content: dict[str, Any]):
-        return cls(200, content)
-    
-    @classmethod
-    def bad_request(cls, error_message: str):
-        return cls(400, { "error": "Bad Request. " + error_message })
-
-    @classmethod
-    def error(cls, error_message: str):
-        return cls(500, { "error": "Internal Server Error. " + error_message })
-
-    def to_return(self):
-        return (str(self.status_code), json.dumps(self.content, ensure_ascii = False))
 
 
-# 個別API
 
+### API ###
 # サンプル： パラメータのバリデーションあり
 class Message(BaseModel):
     message: str
@@ -89,20 +39,6 @@ def log_unsafe(message: dict[str, str]):
     LOGGER.info(response_message)
     return Response.ok({"message": response_message})
 
-
-# エンドポイント
-def api_endpoint(path: str, json_param: str) -> str:
-    response: Response
-    try:
-        data: dict[str, Any] = json.loads(json_param)
-        response = APP.call_route(path, data)
-    except json.JSONDecodeError as e:
-        LOGGER.fatal(f"JSONデコードエラー： {e}")
-        response = Response.bad_request(f"無効なJSON形式です： {str(e)}")
-    except Exception as e:
-        LOGGER.fatal(f"予期せぬエラーが発生しました： {e}")
-        response = Response.error(str(e))
-    return response.to_return()
 
 # APIエンドポイントをwindowに登録(必須)
 js.window.api_endpoint = api_endpoint
